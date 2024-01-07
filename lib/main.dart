be48bb3680ee9f099e4209e9800ee7137cc37278
Romario Negreiros <nromario482@gmail.com>
2024-01-07 20:15:21 -0300

    Add gesture detectors, and mechanism of randomly selecting an option for the app and the selection for the user

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,7 @@
 // ignore_for_file: prefer_const_constructors
 
+import "dart:math";
+
 import "package:flutter/material.dart";
 
 void main() {
@@ -27,6 +29,17 @@ class Home extends StatefulWidget {
 }
 
 class _HomeState extends State<Home> {
+  List<String> options = ["rock", "paper", "scissor"];
+  String userChoice = "";
+  String appChoice = "";
+
+  void handleOptionChoosed(int optionIndex) {
+    setState(() {
+      userChoice = options[optionIndex];
+      appChoice = options[Random().nextInt(options.length + 1)];
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -39,7 +52,12 @@ class _HomeState extends State<Home> {
               padding: EdgeInsets.only(top: 32, bottom: 16),
               child: Text("App choice:",
                   style: TextStyle(fontSize: 20, fontWeight: FontWeight.bold))),
-          Image.asset("assets/images/default.png"),
+          switch (appChoice) {
+            "rock" => Image.asset("assets/images/rock.png"),
+            "paper" => Image.asset("assets/images/paper.png"),
+            "scissor" => Image.asset("assets/images/scissor.png"),
+            _ => Image.asset("assets/images/default.png"),
+          },
           Padding(
               padding: EdgeInsets.only(top: 32, bottom: 16),
               child: Text("Choose one of the options below:",
@@ -47,11 +65,36 @@ class _HomeState extends State<Home> {
           Row(
             mainAxisAlignment: MainAxisAlignment.spaceEvenly,
             children: [
-              Image.asset("assets/images/rock.png", height: 95),
-              Image.asset("assets/images/paper.png", height: 95),
-              Image.asset("assets/images/scissor.png", height: 95),
+              GestureDetector(
+                child: Image.asset(
+                  "assets/images/rock.png",
+                  height: 95,
+                  opacity: userChoice == "" ? null : userChoice != options[0] ? AlwaysStoppedAnimation(.7) : null),
+                onTap: () {
+                  handleOptionChoosed(0);
+                },
+              ),
+              GestureDetector(
+                child: Image.asset(
+                  "assets/images/paper.png",
+                  height: 95,
+                  opacity: userChoice == "" ? null : userChoice != options[1] ? AlwaysStoppedAnimation(.7) : null),
+                onTap: () {
+                  handleOptionChoosed(1);
+                },
+              ),
+              GestureDetector(
+                child: Image.asset(
+                  "assets/images/scissor.png",
+                  height: 95,
+                  opacity: userChoice == "" ? null : userChoice != options[2] ? AlwaysStoppedAnimation(.7) : null),
+                onTap: () {
+                  handleOptionChoosed(2);
+                },
+              ),
             ],
-          )
+          ),
+          
         ]));
   }
 }
